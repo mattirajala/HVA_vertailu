196fb5ef64b862944ba58866d2c846d4464d62d5
Matti Rajala <mattimrajala@gmail.com>
2024-03-25 10:34:15 +0200

    Change theme and add inputs

--- R scripts/ui.R
+++ R scripts/ui.R
@@ -11,31 +11,34 @@ library(shiny)
 source('data.R')
 
 
-alueet = Region_Names_HVA()
 indikaattorit = Indicator_Ids()
 # Define UI for application that draws a histogram
 navbarPage(
 
     # Application title
     "Hyvinvointialueiden vertailu",
-    
+    theme =  bslib::bs_theme(bootswatch = 'flatly'),
     tabPanel("Sotkanet", 
              
              sidebarLayout(
                  sidebarPanel(
                      
                      
-                     selectInput("HVA", "Hyvinvointialue", alueet, multiple = T),
-                     selectInput("ID", "Indikaattori", indikaattorit),
+                     # selectInput("HVA", "Hyvinvointialue", alueet, multiple = T),
+                     uiOutput('HVA_valinta'),
+                     uiOutput('GROUP_ID'),
+                     selectInput("GROUP_ID_2", "Osa-alue", choices = NULL),
+                     selectInput("ID", "Indikaattori", choices = NULL),
+                     # selectInput("ID_2", "Indikaattori", indikaattorit),
                      sliderInput("VUOSI", "Valitse vuosi", min = 2000, max = year(Sys.Date())-1, step = 1, value = c(2010,2023), sep = ""),
-                     
+                     radioButtons("LAN", "Language", choices = c("FI" = "title.fi", "SV" = "title.sv", 'EN' = 'title.en'))
                      
                  ),
                  
                  # Show a plot of the generated distribution
                  mainPanel(
-                     radioButtons("LAN", "Language", choices = c("FI" = "title.fi")),
-                     plotOutput("newPlot"),
+                    
+                     plotOutput("newPlot", width = 'auto', height = 600),
                      tableOutput("table"),
                      textOutput("text"),
                      textOutput("text2")
